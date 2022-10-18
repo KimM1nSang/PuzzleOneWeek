20ed5ec3e6a601459de50c174561c66ae6313761
KimM1nSang <minsang1008@naver.com>
2022-10-18 11:18:48 +0900

    클래스만 만들었음

---
+++ Assets/01.Scripts/Map/Map.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : Singleton<Map>
+{
+    //CCSize _screenSize;
+    //
+    //CCAction* _enemyMove;
+    //CCAction* _treeMove;
+    //
+    //CCArray* _gameCoins;
+
+    int BOARDX = 7;
+    int BOARDY = 6;
+    int TOTALCOINTYPE = 4;
+
+    public bool InitMap()
+    {
+        return true;
+    }
+
+
+    public void createGameScene()
+    {
+
+    }
+    public void createGameAction()
+    {
+
+    }
+    public void initGameCoin()
+    {
+
+    }
+    public void createGameCoin(Vector3 pos, int type, int state)
+    {
+
+    }
+}
