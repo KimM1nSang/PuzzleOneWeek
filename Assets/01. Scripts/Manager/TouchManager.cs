6f57152c020a22a18f900da1aaa8269271d4aff5
vndfjsl <28alsrb@gmail.com>
2022-10-18 18:40:24 +0900

    수정중

---
+++ Assets/01. Scripts/Manager/TouchManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
--- Assets/01. Scripts/Tile/Tile.cs
+++ Assets/01. Scripts/Tile/Tile.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Tile : MonoBehaviour
+public class Tile : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
 {
     public Define.TileState TileState { get; private set; }
     public Define.TileType TileType { get; private set; }
@@ -11,5 +12,25 @@ public class Tile : MonoBehaviour
     {
         this.TileState = state;
         this.TileType = type;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("타일에 마우스를 댐");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("타일에서 마우스를 뗌");
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log("타일을 클릭함");
     }
 }
